25c1b93c4757c906b290606a5de7ae1568397c64
Ayotomiwadavid <ayotomiwadavid@proton.me>
2025-09-14 22:07:20 +0100

    saving progress

--- server/routes/userRoutess.js
+++ server/routes/userRoutess.js
@@ -1,4 +1,4 @@
-const {getAllUsers, getUserById, updateUserById} = require('../controllers/user/userController');
+const {getAllUsers, getUserById, updateUserById, searchDoctors} = require('../controllers/user/userController');
 const middleware = require('../middleware/authMiddleware');
 const express = require('express');
 const router = express.Router();
@@ -7,5 +7,8 @@ const router = express.Router();
 router.get('/', middleware, getAllUsers);
 router.get('/:id', middleware, getUserById);
 router.put('/:id', middleware, updateUserById);
+
+// Route for searching doctors
+router.get('/search/doctors', middleware, searchDoctors);
 
 module.exports = router;
